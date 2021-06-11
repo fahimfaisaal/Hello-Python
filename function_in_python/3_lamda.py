169001c94ba115e570ea224e2fe560822ee20582
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-06-11 21:44:16 +0600

    feat: add lamda function

---
+++ function_in_python/3_lamda.py
@@ -0,0 +1,22 @@
+def calculator(func, *numbers):
+    result: int = 0
+
+    for number in numbers:
+        result += func(number)
+
+    return result
+
+
+def filters(numbers: list, func):
+    evens = []
+    for number in numbers:
+        if func(number):
+            evens.append(number)
+    return evens
+
+
+if __name__ == '__main__':
+    # total = calculator(lambda number: number * 2, 1, 2, 3, 4, 10)
+    # print(total)
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(filters(numbers, lambda num: num % 2 == 1))
